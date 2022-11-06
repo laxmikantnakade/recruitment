da7aba2da5105e508081a057a8d8240995ade340
laxmikantnakade <laxmikantn004@gmail.com>
2022-11-06 16:26:33 +0530

    first commit

---
+++ src/app/services/customvalidation.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {ValidatorFn,AbstractControl,FormGroup} from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomvalidationService {
+
+  constructor() { }
+
+}
